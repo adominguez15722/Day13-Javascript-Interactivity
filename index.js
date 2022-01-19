10dc6b5097f75ce2948902dde8ff5ef0fc7bfbf8
Anthony <adominguez15722@gmail.com>
2022-01-19 15:33:12 -0600

    Step 2 is solved

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 console.log('hello world');
+
+function addMovie(event)  {
+    
+    event.preventDefault()
+    const inputField = document.querySelector('input');
+    let movie = document.createElement('li');
+    let movieTitle = document.createElement('span');
+    
+    movieTitle.textContent = inputField.value;
+    movie.appendChild(movieTitle);
+
+    document.querySelector('ul').appendChild(movie);
+
+
+    // function clear()    {
+    //     inputField.value = ''        
+    // }
+    // document.querySelector('button').addEventListener('submit', clear)
+}
+
+document.querySelector('form').addEventListener('submit', addMovie);
+
+
+
